c3406acbd919c43e176087e9b11cb189ce6fd790
Chaarlessantos <charlesfarmaanddev@gmail.com>
2024-10-22 18:17:59 -0300

    Atualizacao final dio

--- desafio_gft.js
+++ desafio_gft.js
@@ -1,1 +1,33 @@
-console.log("oi")
+let nomeHeroi = "Charles";
+let xpHeroi = 8888;
+
+let nivel = '';
+
+
+if (xpHeroi < 1000) {
+    nivel = 'Ferro';
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = 'Bronze';
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = 'Prata';
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = 'Ouro';
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = 'Platina';
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = 'Ascendente';
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = 'Imortal';
+} else if (xpHeroi >= 10001) {
+    nivel = 'Radiante';
+}
+
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+
+
+if (nivel === 'Ascendente' || nivel === 'Imortal' || nivel === 'Radiante') {
+    console.log(`O Herói de nome ${nomeHeroi} está apto a disputar o campeonato!`);
+} else {
+    console.log(`O Herói de nome ${nomeHeroi} não está apto a disputar o campeonato.`);
+}
